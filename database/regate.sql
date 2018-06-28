7b61b6b43102d7913dedc07d94b40e4f6e9f068c
QYing <ying.qixin@gmail.com>
2018-06-28 15:23:43 +0200

    update database

--- database/regate.sql
+++ database/regate.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le :  mer. 27 juin 2018 à 16:20
--- Version du serveur :  5.6.35
--- Version de PHP :  7.1.8
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 28 juin 2018 à 13:23
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `regate`
@@ -20,23 +28,27 @@ SET time_zone = "+00:00";
 -- Structure de la table `bateau`
 --
 
-CREATE TABLE `bateau` (
-  `bateau_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `bateau`;
+CREATE TABLE IF NOT EXISTS `bateau` (
+  `bateau_id` int(11) NOT NULL AUTO_INCREMENT,
   `serie` varchar(255) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `numVoile` int(11) NOT NULL,
   `equipiers` text,
-  `coefficient` float NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `coefficient` float NOT NULL DEFAULT '1',
+  PRIMARY KEY (`bateau_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `bateau`
 --
 
 INSERT INTO `bateau` (`bateau_id`, `serie`, `nom`, `numVoile`, `equipiers`, `coefficient`) VALUES
-(9, 'test', 'Qixin', 112, 'a:1:{i:4;s:19:\"Suliman Abdergadeir\";}', 0),
-(13, 'dfdgc', 'tgfcgfdgh', 5659, 'a:1:{i:6;s:13:\"Macron Manuel\";}', 0),
-(12, 'test', 'trrr', 65, 'a:0:{}', 0);
+(9, '', 'Qixin', 112, 'a:0:{}', 1),
+(13, '', 'tgfcgfdgh', 5659, '', 1),
+(12, '', 'trrr', 65, 'a:0:{}', 1),
+(14, '', 'Morgane', 123, 'a:1:{i:4;s:19:\"Suliman Abdergadeir\";}', 1),
+(16, '', 'XlmTEST', 44, 'a:0:{}', 1);
 
 -- --------------------------------------------------------
 
@@ -44,9 +56,12 @@ INSERT INTO `bateau` (`bateau_id`, `serie`, `nom`, `numVoile`, `equipiers`, `coe
 -- Structure de la table `bateauequipier`
 --
 
-CREATE TABLE `bateauequipier` (
+DROP TABLE IF EXISTS `bateauequipier`;
+CREATE TABLE IF NOT EXISTS `bateauequipier` (
   `bateau` int(10) UNSIGNED NOT NULL,
-  `equipier` int(10) UNSIGNED NOT NULL
+  `equipier` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`bateau`,`equipier`),
+  KEY `Constr_bateauEquipier_equipier_fk` (`equipier`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -55,22 +70,22 @@ CREATE TABLE `bateauequipier` (
 -- Structure de la table `equipier`
 --
 
-CREATE TABLE `equipier` (
-  `equipier_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `equipier`;
+CREATE TABLE IF NOT EXISTS `equipier` (
+  `equipier_id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
-  `occupe` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id_bateau` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`equipier_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `equipier`
 --
 
-INSERT INTO `equipier` (`equipier_id`, `nom`, `prenom`, `role`, `occupe`) VALUES
-(4, 'Abdergadeir', 'Suliman', 'Voilier', 1),
-(6, 'Manuel', 'Macron', 'TEst', 1),
-(7, 'rersr', 'rererere', 'TEst', 0);
+INSERT INTO `equipier` (`equipier_id`, `nom`, `prenom`, `role`, `id_bateau`) VALUES
+(4, 'Abdergadeir', 'Suliman', 'Voilier', 14);
 
 -- --------------------------------------------------------
 
@@ -78,8 +93,9 @@ INSERT INTO `equipier` (`equipier_id`, `nom`, `prenom`, `role`, `occupe`) VALUES
 -- Structure de la table `regate`
 --
 
-CREATE TABLE `regate` (
-  `regate_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `regate`;
+CREATE TABLE IF NOT EXISTS `regate` (
+  `regate_id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `club` varchar(255) NOT NULL,
@@ -90,8 +106,9 @@ CREATE TABLE `regate` (
   `officierDeJour` varchar(255) NOT NULL,
   `etape` varchar(255) NOT NULL DEFAULT '0',
   `heure_dep` time DEFAULT NULL,
-  `heure_arr` time DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `heure_arr` time DEFAULT NULL,
+  PRIMARY KEY (`regate_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `regate`
@@ -100,10 +117,26 @@ CREATE TABLE `regate` (
 INSERT INTO `regate` (`regate_id`, `nom`, `date`, `club`, `ligue`, `jury`, `comiteDeCourse`, `securite`, `officierDeJour`, `etape`, `heure_dep`, `heure_arr`) VALUES
 (1, 'Oui', '2018-06-26', 'Barcelone', 'Europa', 'Jean, guillaume', 'Pierre', 'Jean', 'CHarles', '0', NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `regatebateau`
+--
+
+DROP TABLE IF EXISTS `regatebateau`;
+CREATE TABLE IF NOT EXISTS `regatebateau` (
+  `regate` int(10) UNSIGNED NOT NULL,
+  `bateau` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`regate`,`bateau`),
+  KEY `Constr_regateBateau_bateau_fk` (`bateau`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `seriebateau`
 --
+
 DROP TABLE IF EXISTS `seriebateau`;
 CREATE TABLE IF NOT EXISTS `seriebateau` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -111,80 +144,9 @@ CREATE TABLE IF NOT EXISTS `seriebateau` (
   `coeff` float NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `seriebateau`
---
-
-INSERT INTO `seriebateau` (`id`, `type`, `coeff`) VALUES
-(1, 'XLM52', 2.1),
-(3, 'Jolie morgane', 1.5);
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 COMMIT;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `regatebateau`
---
-
-CREATE TABLE `regatebateau` (
-  `regate` int(10) UNSIGNED NOT NULL,
-  `bateau` int(10) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `bateau`
---
-ALTER TABLE `bateau`
-  ADD PRIMARY KEY (`bateau_id`);
-
---
--- Index pour la table `bateauequipier`
---
-ALTER TABLE `bateauequipier`
-  ADD PRIMARY KEY (`bateau`,`equipier`),
-  ADD KEY `Constr_bateauEquipier_equipier_fk` (`equipier`);
-
---
--- Index pour la table `equipier`
---
-ALTER TABLE `equipier`
-  ADD PRIMARY KEY (`equipier_id`);
-
---
--- Index pour la table `regate`
---
-ALTER TABLE `regate`
-  ADD PRIMARY KEY (`regate_id`);
-
---
--- Index pour la table `regatebateau`
---
-ALTER TABLE `regatebateau`
-  ADD PRIMARY KEY (`regate`,`bateau`),
-  ADD KEY `Constr_regateBateau_bateau_fk` (`bateau`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `bateau`
---
-ALTER TABLE `bateau`
-  MODIFY `bateau_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT pour la table `equipier`
---
-ALTER TABLE `equipier`
-  MODIFY `equipier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT pour la table `regate`
---
-ALTER TABLE `regate`
-  MODIFY `regate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
