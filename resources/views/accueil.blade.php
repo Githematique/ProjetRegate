1e831dc8b1888ecbb32b90fa13224f24242729ef
QYing <ying.qixin@gmail.com>
2018-07-24 01:21:51 +0200

    fix mobile design

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,52 +4,53 @@
 
 @section('content')
 
-  <div class="container">
-    <div class="blockStatus col-6 text-center">
-<!--       <img class="gif" src="https://media.giphy.com/media/3o7aCVTfelG4XSbv3y/giphy.gif" alt="gif">
- -->    </div>
+<div class="container home-container">
+  <div class="blockStatus col-6 text-center">
+    <!--       <img class="gif" src="https://media.giphy.com/media/3o7aCVTfelG4XSbv3y/giphy.gif" alt="gif">
+ --></div>
 
-    <div class="col-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">Status de la régate : <font style="text-transform: uppercase;">{!! $regate->etape !!}</font></div>
-        <div class="panel-body">
-          <i id="led1" class="led1"></i>
-          <i id="led2" class="led2"></i>
-          <i id="led3" class="led3"></i>
-          <i id="led4" class="led4"></i>
-          <i id="petitSon" class="fas fa-volume-down petitSon"></i>
-          <i id="grandSon" class="fas fa-volume-up grandSon"></i>
+  <div class="col-6 led-container">
+    <div class="panel panel-default">
+      <div class="panel-heading">Status de la régate :
+        <font style="text-transform: uppercase;">{!! $regate->etape !!}</font>
+      </div>
+      <div class="panel-body">
+        <i id="led1" class="led1"></i>
+        <i id="led2" class="led2"></i>
+        <i id="led3" class="led3"></i>
+        <i id="led4" class="led4"></i>
+        <i id="petitSon" class="fas fa-volume-down petitSon"></i>
+        <i id="grandSon" class="fas fa-volume-up grandSon"></i>
+      </div>
+    </div>
+  </div>
+  <form method="get" action="/ledOn" enctype="multipart/form-data">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div class="flex">
+        <div id="apercu" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container apercu">
+          <button  type="submit" class="btn" name="apercu">Apercu</button>
+        </div>
+        <div  id="depart" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container depart ">
+          <button type="submit" class="btn " name="depart">Départ</button>
+        </div>
+        <div id="rappel_g" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container rappel_g hide">
+          <button  type="submit" class="btn " name="rappel_g">Rappel</br>général</button>
+        </div>
+        <div id="rappel_i" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container rappel_i hide">
+          <button  type="submit" class="btn " name="rappel_i">Rappel </br>individuel</button>
+        </div>
+        <div id="arrivee" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container arrivee hide">
+          <button type="submit" class="btn" name="arrivee">Arrivée</button>
+        </div>
+        <div id="annulation" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container annulation hide">
+          <button  type="submit" class="btn" name="annulation">Annulation</button>
+        </div>
+        <div id="modification" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 btn-container  modification hide">
+          <button  type="submit" class="btn" name="modification">Modification </br>de parcours</button>
         </div>
       </div>
     </div>
-<form method="get" action="/ledOn" enctype="multipart/form-data">
-        
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-	  <div class="flex">
-	    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	      <button id="apercu" type="submit" class="btn apercu" name="apercu">Apercu</button>
-	    </div>
-	    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	      <button id="depart" type="submit" class="btn depart" name="depart">Départ</button>
-	    </div>
-	    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	      <button id="rappel_g" type="submit" class="btn rappel_g hide" name="rappel_g">Rappel</br>général</button>
-	    </div>
-	    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	      <button id="rappel_i" type="submit" class="btn rappel_i hide" name="rappel_i">Rappel </br>individuel</button>
-	    </div>
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <button id="arrivee" type="submit" class="btn arrivee hide" name="arrivee">Arrivée</button>
-      </div>  
-	    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	      <button id="annulation" type="submit" class="btn annulation hide" name="annulation">Annulation</button>
-	    </div>   
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <button id="modification" type="submit" class="btn modification hide" name="modification">Modification </br>de parcours</button>
-      </div>   
-    </div> 
-   </div>
-</form>
+  </form>
 </div>
 <script type="text/javascript">
   var test = '{!! $regate->etape !!}';
@@ -65,7 +66,7 @@
     document.getElementById("arrivee").classList.add("hide");
     document.getElementById("annulation").classList.add("hide");
     document.getElementById("modification").classList.add("hide");
-  }else if (test == 'depart'){
+  } else if (test == 'depart') {
     var heure_dep = new Date().toLocaleTimeString();
     document.getElementById("led1").style.background = '#ff0000';
     document.getElementById("led3").style.background = '#ff9900';
@@ -77,7 +78,7 @@
     document.getElementById("arrivee").classList.remove("hide");
     document.getElementById("annulation").classList.remove("hide");
     document.getElementById("modification").classList.remove("hide");
-  }else if (test == 'rappel_i') {
+  } else if (test == 'rappel_i') {
     document.getElementById("led2").style.background = '#0066ff';
     document.getElementById("led3").style.background = '#ff9900';
     document.getElementById("grandSon").style.color = '#ff0000';
@@ -88,7 +89,7 @@
     document.getElementById("arrivee").classList.remove("hide");
     document.getElementById("annulation").classList.remove("hide");
     document.getElementById("modification").classList.remove("hide");
-  }else if (test == 'rappel_g') {
+  } else if (test == 'rappel_g') {
     document.getElementById("led1").style.background = '#ff0000';
     document.getElementById("led2").style.background = '#0066ff';
     document.getElementById("petitSon").style.color = '#ff0000';
@@ -100,7 +101,7 @@
     document.getElementById("arrivee").classList.remove("hide");
     document.getElementById("annulation").classList.remove("hide");
     document.getElementById("modification").classList.remove("hide");
-  }else if (test == 'arrivee') {
+  } else if (test == 'arrivee') {
     var heure_arr = new Date().toLocaleTimeString();
     document.getElementById("apercu").classList.remove("hide");
     document.getElementById("depart").classList.remove("hide");
@@ -109,7 +110,7 @@
     document.getElementById("arrivee").classList.add("hide");
     document.getElementById("annulation").classList.add("hide");
     document.getElementById("modification").classList.add("hide");
-  }else if (test == 'annulation') {
+  } else if (test == 'annulation') {
     document.getElementById("led1").style.background = '#ff0000';
     document.getElementById("led2").style.background = '#0066ff';
     document.getElementById("led3").style.background = '#ff9900';
@@ -122,8 +123,8 @@
     document.getElementById("arrivee").classList.add("hide");
     document.getElementById("annulation").classList.add("hide");
     document.getElementById("modification").classList.add("hide");
-  }else if (test == 'modification') {
-        document.getElementById("led1").style.background = '#ff0000';
+  } else if (test == 'modification') {
+    document.getElementById("led1").style.background = '#ff0000';
     document.getElementById("led2").style.background = '#0066ff';
     document.getElementById("led3").style.background = '#ff9900';
     document.getElementById("petitSon").style.color = '#ff0000';
@@ -137,4 +138,3 @@
     document.getElementById("modification").classList.add("hide");
   }
 </script>
-
